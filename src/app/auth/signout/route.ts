b1d1f0bce0323babfadaa4824764e884709d46ef
Kirill Bezzubkin <pimp.my.bi@gmail.com>
2025-11-30 18:43:18 +0300

    Feat: Add signout route handler

---
+++ src/app/auth/signout/route.ts
@@ -0,0 +1,37 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+    const cookieStore = await cookies()
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    cookieStore.set({ name, value, ...options })
+                },
+                remove(name: string, options: CookieOptions) {
+                    cookieStore.delete({ name, ...options })
+                },
+            },
+        }
+    )
+
+    // Check if we have a session
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+
+    if (session) {
+        await supabase.auth.signOut()
+    }
+
+    return NextResponse.redirect(new URL('/login', request.url), {
+        status: 302,
+    })
+}
